c477c3b2d412a6dbc7596fa70f98538474d0b23a
Dylan Tivnan <tivnand@gmail.com>
2023-02-07 10:47:57 -0500

    Updated PlayerController.cs
    Added Boost with Key w and added a basespeed.

--- SnowboardGame/Assets/Scripts/PlayerController.cs
+++ SnowboardGame/Assets/Scripts/PlayerController.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float baseAmount = 20f;
+    [SerializeField] float boostAmount = 30f;
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    void RotatePlayer()
     {
         if(Input.GetKey(KeyCode.A))
         {
@@ -22,6 +32,19 @@ public class PlayerController : MonoBehaviour
         else if(Input.GetKey(KeyCode.D))
         {
             rb2d.AddTorque(-torqueAmount);
+        }
+    }
+
+    void RespondToBoost()
+    {
+        //up = boost 
+        if(Input.GetKey(KeyCode.W))
+        {
+            surfaceEffector2D.speed = boostAmount;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseAmount;
         }
     }
 }
